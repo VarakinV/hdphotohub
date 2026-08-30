cde08a97bf96967c939673b274a783929b6dc6a9
Vlad Varakin <info@westmanwebdesign.com>
2026-08-30 13:49:53 -0600

    Reel V16 - updated

--- remotion/compositions/reels/ReelV16.tsx
+++ remotion/compositions/reels/ReelV16.tsx
@@ -135,7 +135,7 @@ const TextOverlay: React.FC<{
         style={{
           position: 'absolute',
           left: 365,
-          top: 525,
+          top: 625,
           width: 350,
           height: 350,
           borderRadius: '50%',
@@ -158,7 +158,7 @@ const TextOverlay: React.FC<{
       <div
         style={{
           position: 'absolute',
-          top: 930,
+          top: 1030,
           left: 0,
           right: 0,
           textAlign: 'center',
@@ -177,7 +177,7 @@ const TextOverlay: React.FC<{
       <div
         style={{
           position: 'absolute',
-          top: 1030,
+          top: 1130,
           left: 0,
           right: 0,
           textAlign: 'center',
@@ -198,7 +198,7 @@ const TextOverlay: React.FC<{
         style={{
           position: 'absolute',
           left: 369,
-          top: 1200,
+          top: 1300,
           width: 342,
           height: 125,
           objectFit: 'contain',
